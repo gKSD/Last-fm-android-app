adca6c63eb425551ce0a90bf1cab502107a7ba30
Stepan <drstep321@mail.ru>
2014-04-17 08:58:56 +0400

    Init commit

--- Last.fm/src/main/last/fm/LoginActivity.java
+++ Last.fm/src/main/last/fm/LoginActivity.java
@@ -1,15 +1,32 @@
 package main.last.fm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends Activity {
+    private LastFmServiceHelper lfServiceHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Button comeInBtn = (Button)findViewById(R.id.button);
+        lfServiceHelper = LastFmServiceHelper.getInstance();
+        comeInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText loginEdt = (EditText)findViewById(R.id.editText1);
+                EditText passwdEdt = (EditText)findViewById(R.id.editText2);
+                String login = loginEdt.getText().toString();
+                String passwd = passwdEdt.getText().toString();
+                lfServiceHelper.getAuthParams(login,passwd);
+            }
+        });
     }
 
 
