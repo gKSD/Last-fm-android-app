bbf0d473a595131e3a2d179a42c4101c34b932a3
Sofia Kiseleva (gKSD) <Sofia-kiseleva@mail.ru>
2014-05-21 02:21:26 +0400

    fragments added 2

--- Last.fm/src/main/last/fm/activities/LoginActivity.java
+++ Last.fm/src/main/last/fm/activities/LoginActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import main.last.fm.content.provider.LastFmDatabaseHelper;
 import main.last.fm.content.provider.LastFmMainData;
 import main.last.fm.service.LastFmServiceHelper;
 import main.last.fm.R;
@@ -20,13 +19,22 @@ public class LoginActivity extends Activity {
 
     private static final String LOG_TAG = "LoginActivity";
 
-    private LastFmServiceHelper lfServiceHelper;
+    private LastFmServiceHelper lastFmServiceHelper;
     private int ACTIVITY_ID = 0;
+
+    public final LastFmServiceHelper getLastFmServiceHelper() {
+        return lastFmServiceHelper;
+    }
+
+    public final int getActivityId()
+    {
+        return ACTIVITY_ID;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.fragment_login);
 
         //******************************************************************************************************************************************************************************
         //пример запроса в бд
@@ -68,18 +76,22 @@ public class LoginActivity extends Activity {
         cursor.close();
         //******************************************************************************************************************************************************************************
 
-        final LoginActivity ptr = this;
+
+        lastFmServiceHelper = LastFmServiceHelper.getInstance();
+        final LoginActivity ptr = (LoginActivity) this;
+
 
         Button comeInBtn = (Button)findViewById(R.id.button);
-        lfServiceHelper = LastFmServiceHelper.getInstance();
+
         comeInBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText loginEdt = (EditText) findViewById(R.id.editText1);
-                EditText passwdEdt = (EditText) findViewById(R.id.editText2);
+                Log.i(LOG_TAG, "111111111111111111111111111111111111111111111111111111111111111");
+                EditText loginEdt = (EditText) ptr.findViewById(R.id.editText1);
+                EditText passwdEdt = (EditText) ptr.findViewById(R.id.editText2);
                 String login = loginEdt.getText().toString();
                 String passwd = passwdEdt.getText().toString();
-                lfServiceHelper.authIntent(ptr, login, passwd, ACTIVITY_ID);
+                ptr.getLastFmServiceHelper().authIntent(ptr, login, passwd, ACTIVITY_ID);
             }
         });
     }
@@ -88,6 +100,7 @@ public class LoginActivity extends Activity {
     public boolean onCreateOptionsMenu(Menu menu) {
         //getMenuInflater().inflate(R.menu.login, menu);
         //return true;
+        Log.d(LOG_TAG, "onCreateOptionsMenu() ia called");
         return false;
     }
     
