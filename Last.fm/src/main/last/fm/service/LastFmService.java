873d52fb06802a0f2303ec022b02f5f75d7efd6c
Sofia Kiseleva (gKSD) <Sofia-kiseleva@mail.ru>
2014-05-21 16:49:54 +0400

    Stupid F.

--- Last.fm/src/main/last/fm/service/LastFmService.java
+++ Last.fm/src/main/last/fm/service/LastFmService.java
@@ -1,35 +1,8 @@
 package main.last.fm.service;
 
 import android.app.IntentService;
-import android.app.Service;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Binder;
-import android.os.IBinder;
 import android.os.ResultReceiver;
-import android.util.Log;
-
-import org.apache.http.util.ByteArrayBuffer;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.Provider;
-
-import main.last.fm.content.provider.LastFmMainData;
-import main.last.fm.processor.LoginProcessor;
-import main.last.fm.webservice.RestExecutor;
 
 /**
  * Created by step on 17.04.14.
@@ -39,7 +12,7 @@ public class LastFmService extends IntentService {
     private static final String LOG_TAG = "LastFmService";
     String REQUEST_URL = "https://ws.audioscrobbler.com/2.0/?method=";
     String API_K = "544aa2e6717625cc3fd72da91fcfa7df";
-    public ServiceProcessor sp = new ServiceProcessor();
+    public ServiceProcessor serviceProcessor = new ServiceProcessor();
 
     public static final int SERVICE_STATUS_OK = 0;
     public static final int SERVICE_STATUS_ERROR = 1;
@@ -71,7 +44,7 @@ public class LastFmService extends IntentService {
         {
             case 0:
                 PostParams = intent.getStringExtra("PostAuth");
-                sp.ProcessLogin(PostParams);
+                serviceProcessor.ProcessLogin(PostParams);
         }
         //LoginProcessor processor = new LoginProcessor(response);
 
