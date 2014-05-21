03f64d36656a88167d722268c67f918a824ee0b3
Sofia Kiseleva (gKSD) <Sofia-kiseleva@mail.ru>
2014-05-21 20:47:50 +0400

    qwerty

--- Last.fm/src/main/last/fm/activities/fragments/MainScreenFragment.java
+++ Last.fm/src/main/last/fm/activities/fragments/MainScreenFragment.java
@@ -2,11 +2,14 @@ package main.last.fm.activities.fragments;
 
 import android.app.ActionBar;
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import main.last.fm.R;
 import main.last.fm.service.ServiceResultReceiver;
@@ -18,11 +21,45 @@ public class MainScreenFragment extends Fragment  implements ServiceResultReceiv
 
     private static final String LOG_TAG = "MainScreenFragment";
 
+    private ServiceResultReceiver resultReceiver;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         // TODO Auto-generated method stub
         super.onCreate(savedInstanceState);
+
+        resultReceiver = new ServiceResultReceiver(new Handler());
+        resultReceiver.setReceiver(this);
+
+
+        Button button1 = (Button)getActivity().findViewById(R.id.moreButton);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                //startActivity(new Intent(MainActivity.this, ActivityOk.class));
+            }
+        });
+
+        Button button2 = (Button)getActivity().findViewById(R.id.moreButton2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                //startActivity(new Intent(MainActivity.this, ActivityOk.class));
+            }
+        });
+
+
+        /*Button button3 = (Button)getActivity().findViewById(R.id.moreButton3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+
+            }
+        });*/
 
 
 
