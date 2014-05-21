b8ab300c079e788148c351092f8a801a39f41ea4
Sofia Kiseleva (gKSD) <Sofia-kiseleva@mail.ru>
2014-05-21 15:00:44 +0400

    .

--- Last.fm/src/main/last/fm/activities/fragments/LoginFragment.java
+++ Last.fm/src/main/last/fm/activities/fragments/LoginFragment.java
@@ -1,21 +1,26 @@
 package main.last.fm.activities.fragments;
 
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import main.last.fm.R;
+import main.last.fm.activities.LoginActivity;
+import main.last.fm.service.LastFmServiceHelper;
 
 /**
- * Created by sofia on 20.05.14.
+ * Created by sofia on 21.05.14.
  */
-public class MainScreenFragment extends Fragment {
+public class LoginFragment extends Fragment {
 
-    private static final String LOG_TAG = "MainScreenFragment";
-
+    private static final String LOG_TAG = "LoginFragment";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
